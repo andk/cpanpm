e3de3820685829c798473770242cae5b345a4cb1
Andreas König <andk@cpan.org>
2005-11-02 03:52:52 +0000

    it was not stderr, it was stdout; now matching against explicit string
    git-svn-id: https://pause.perl.org:5460/svn/cpanpm/trunk@209 81f69c26-07c4-0310-b1c3-f486c8728884

--- release-lib/Module/Release/ANDK.pm
+++ release-lib/Module/Release/ANDK.pm
@@ -16,12 +16,10 @@ our $VERSION = "0.001";
 
 sub dist {
   my($self) = @_;
-  my $messages = $self->run( "$self->{make} what-is-the-release-name" );
-  $messages =~ s/^\s+//;
-  $messages =~ s/\s+\z//;
-  warn "messages[$messages]";
-  die if $messages =~ /\s/;
-  $self->{local} = $self->{remote} = $messages;
+  my $messages = $self->run( "$self->{make} what-is-the-release-name 2>&1" );
+  my($release_name) = $messages =~ /release-name-is:\s+(.+)/;
+  die "bad release name[$release_name]" if $release_name =~ /\s/ or $release_name !~ /\.tar\.gz$/;
+  $self->{local} = $self->{remote} = $release_name;
 }
 
 1;
