71682336eaa690f1061d28664d5f8d8e75e50e68
Andreas König <andk@cpan.org>
2005-11-02 03:57:15 +0000

    colon even with ticks not allowed here
    git-svn-id: https://pause.perl.org:5460/svn/cpanpm/trunk@211 81f69c26-07c4-0310-b1c3-f486c8728884

--- release-lib/Module/Release/ANDK.pm
+++ release-lib/Module/Release/ANDK.pm
@@ -17,7 +17,7 @@ our $VERSION = "0.001";
 sub dist {
   my($self) = @_;
   my $messages = $self->run( "$self->{make} what-is-the-release-name 2>&1" );
-  my($release_name) = $messages =~ /release-name-is:\s+(.+)/;
+  my($release_name) = $messages =~ /release-name-is\s+(.+)/;
   die "bad release name[$release_name]" if $release_name =~ /\s/ or $release_name !~ /\.tar\.gz$/;
   $self->{local} = $self->{remote} = $release_name;
 }
