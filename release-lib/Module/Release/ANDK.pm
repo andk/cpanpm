3251c0bdda088c2107e4901e0072dc1ade1fedf3
Andreas König <andk@cpan.org>
2005-11-02 03:42:53 +0000

    release depends on running make dist for finding out the release name; we follow the rule but we do not run make dist when make disttest overwrites the release anyway
    git-svn-id: https://pause.perl.org:5460/svn/cpanpm/trunk@207 81f69c26-07c4-0310-b1c3-f486c8728884

--- release-lib/Module/Release/ANDK.pm
+++ release-lib/Module/Release/ANDK.pm
@@ -14,4 +14,14 @@ our $VERSION = "0.001";
     *clean =
     sub { return };
 
+sub dist {
+  my($self) = @_;
+  my $messages = $self->run( "$self->{make} what-is-the-release-name 2>&1" );
+  $messages =~ s/^\s+//;
+  $messages =~ s/\s+\z//;
+  warn "messages[$messages]";
+  die if $messages =~ /\s/;
+  $self->{local} = $self->{remote} = $messages;
+}
+
 1;
