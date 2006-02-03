4c4410d417b31fd610ad50d59cff250f02d224d5
Andreas König <andk@cpan.org>
2006-02-03 20:03:04 +0000

    refinement
    git-svn-id: https://pause.perl.org:5460/svn/cpanpm/trunk@521 81f69c26-07c4-0310-b1c3-f486c8728884

--- t/CPAN/authors/id/A/AN/ANDK/CPAN-Test-Dummy-Perl5-Make-1.01/lib/CPAN/Test/Dummy/Perl5/Make.pm
+++ t/CPAN/authors/id/A/AN/ANDK/CPAN-Test-Dummy-Perl5-Make-1.01/lib/CPAN/Test/Dummy/Perl5/Make.pm
@@ -18,49 +18,26 @@ __END__
 
 =head1 NAME
 
-CPAN::Test::Dummy::Perl5::Make - CPAN Test Dummy for CPAN Makefile.PL installs
+CPAN::Test::Dummy::Perl5::Make - CPAN Test Dummy
 
 =head1 SYNOPSIS
 
     use CPAN::Test::Dummy::Perl5::Make;
-    
-    my $name = CPAN::Test::Dummy::Perl5::Make->dummy;
 
 =head1 DESCRIPTION
 
-This module is part of the Perl Image Testing Architecture (CPAN) and
-acts as a test module for the L<CPAN::Scheme::Perl5::Make> testing
-scheme.
+This module is part of CPAN.pm with the single purpose of testing
+CPAN.pm itself.
 
-1. Contains no functionality, and will never do so.
-
-2. Has no non-core depencies, and will never have any.
-
-3. Exists on CPAN.
-
-=head1 METHODS
-
-=head2 dummy
-
-Returns the dummy's name, in this case 'Mike'
+Contains no functionality, and will never do so.
 
 =head1 AUTHOR
 
-Adam Kennedy, C<< <cpan at ali.as> >>
-
-=head1 SUPPORT
-
-No support is available for Mike.
-
-OK OK, yes his head B<is> meant to come off like that
-
-=head1 SEE ALSO
-
-L<CPAN>, L<CPAN::Scheme::Perl5::Make>, L<http://ali.as/pita/>
+Andreas Koenig, based PITA by Adam Kennedy.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2006 Adam Kennedy, all rights reserved.
+Copyright 2006 Andreas Koenig
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
