dc1eeac84541d0fa806c1fa79272c57d36fc5111
Andreas König <andk@cpan.org>
2006-08-19 14:09:28 +0000

    blah
    git-svn-id: https://pause.perl.org:5460/svn/cpanpm/trunk@752 81f69c26-07c4-0310-b1c3-f486c8728884

--- bin/cp-to-perl.pl
+++ bin/cp-to-perl.pl
@@ -5,6 +5,19 @@
 Script to copy those files that live also in the core into a perl
 source tree.
 
+I use it such that I first make a clone of bleedperl in repoperls
+directory:
+
+    rsync -ax perl-p-5.8.0@28630/ perl-p-5.8.0@28630-cpan.pm-1.8755/
+
+Then I use cp-to-perl to clobber the clone in CPAN/SVN/ directory:
+
+    perl bin/cp-to-perl.pl /home/src/perl/repoperls/perl-p-5.8.0@28630-cpan.pm-1.8755
+
+And then back in the repoperls directory I do makepatch:
+
+    makepatch --diff 'diff -u' perl-p-5.8.0@28630/ perl-p-5.8.0@28630-cpan.pm-1.8755/ > patch
+
 =cut
 
 use strict;
