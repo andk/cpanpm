1c3feed60f850b3de8c066643ef41c1caa08b683
Andreas König <andk@cpan.org>
2006-10-23 07:09:14 +0000

    backporting business
    git-svn-id: https://pause.perl.org:5460/svn/cpanpm/branches/1.88-maint@1118 81f69c26-07c4-0310-b1c3-f486c8728884

--- bin/no_CR.pl
+++ bin/no_CR.pl
@@ -1,8 +1,8 @@
 while (<>) {
-  die if /\cM/;
+  die "contains CR" if /\cM/;
   next unless /^(\S+)/;
   my $file = $1;
-  open FH, $file or die;
+  open FH, $file or die "Could not open '$file': $!";
   next if -B $file;
   while (<FH>) {
     die "Found CR in $file" if /\cM/;
