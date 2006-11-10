1c14206759ca34c0ee3cc5d9a8248272f4daf409
Andreas König <andk@cpan.org>
2006-11-10 08:32:15 +0000

    prepare 1.88_61
    git-svn-id: https://pause.perl.org:5460/svn/cpanpm/trunk@1244 81f69c26-07c4-0310-b1c3-f486c8728884

--- lib/CPAN.pm
+++ lib/CPAN.pm
@@ -9683,6 +9683,7 @@ defined:
   prefs_dir          local directory to store per-distro build options
   proxy_user         username for accessing an authenticating proxy
   proxy_pass         password for accessing an authenticating proxy
+  randomize_urllist  add some randomness to the sequence of the urllist
   scan_cache	     controls scanning of cache ('atstart' or 'never')
   shell              your favorite shell
   show_upload_date   boolean if commands should try to determine upload date
