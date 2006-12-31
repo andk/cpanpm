8aeaf72fe5d895b6f74f3855809c4d3f5402161e
Andreas König <andk@cpan.org>
2006-12-31 17:12:05 +0000

    r1669@k75:  k | 2006-12-31 18:11:55 +0100  prepare 1.88_67
    git-svn-id: https://pause.perl.org:5460/svn/cpanpm/trunk@1445 81f69c26-07c4-0310-b1c3-f486c8728884

--- lib/CPAN.pm
+++ lib/CPAN.pm
@@ -2515,7 +2515,7 @@ sub expand_by_method {
             for $obj (
                       $CPAN::META->all_objects($class)
                      ) {
-                unless ($obj && $obj->can("id") && $obj->id){
+                unless ($obj && UNIVERSAL::can($obj,"id") && $obj->id){
                     # BUG, we got an empty object somewhere
                     require Data::Dumper;
                     CPAN->debug(sprintf(
