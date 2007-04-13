c05447df75e4f1ccf2c63e216cd3f8ebf621f3a1
Andreas König <andk@cpan.org>
2007-04-13 05:13:54 +0000

    r2209@k75:  k | 2007-04-13 07:12:49 +0200  forgot to add the reports command to the commands table for completion
    git-svn-id: https://pause.perl.org:5460/svn/cpanpm/trunk@1726 81f69c26-07c4-0310-b1c3-f486c8728884

--- lib/CPAN.pm
+++ lib/CPAN.pm
@@ -520,6 +520,7 @@ use strict;
                                     recompile
                                     reload
                                     report
+                                    reports
                                     scripts
                                     test
                                     upgrade
