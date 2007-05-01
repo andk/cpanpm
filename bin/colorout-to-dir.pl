eb9f3dc1fbec666c7755be26ba51a55767a25bd8
Andreas König <andk@cpan.org>
2007-05-01 02:38:32 +0000

    r2311@k75:  k | 2007-05-01 04:34:39 +0200  notes; megainstall now splits its logfile
    git-svn-id: https://pause.perl.org:5460/svn/cpanpm/trunk@1790 81f69c26-07c4-0310-b1c3-f486c8728884

--- bin/colorout-to-dir.pl
+++ bin/colorout-to-dir.pl
@@ -1,9 +1,10 @@
 #!/usr/bin/perl -0777 -nl
 
 use strict;
+use YAML::Syck;
 use File::Path qw(mkpath);
 use List::MoreUtils qw(uniq);
-our($perl_path) = m|(\S+/installed-perls/(?:.*?)/p.*?/perl-5.*?@(?:\d+))|;
+our($perl_path) = m|(/home\S+/installed-perls/(?:.*?)/p.*?/perl-5.*?@(?:\d+))|;
 our $outdir = $ARGV;
 $outdir =~ s/.out$/.d/ or die;
 mkpath $outdir;
@@ -11,11 +12,17 @@ mkpath $outdir;
 sub mystore ($$$){
   my($shortdistro,$log,$ok) = @_;
   my $outfile = $shortdistro;
-  $outfile =~ s!\.(tar.gz|tgz|tar.bz2|tbz|zip)$!!;
+  $outfile =~ s!\.(tar.gz|tgz|tar.bz2|tbz|zip)?$!.xml!;
   $outfile =~ s|/|!|g;
   $outfile =~ s|^|$outdir/|;
   my($time) = $outdir =~ /(\d{8}T\d{4})/;
   open my $fh, ">", $outfile or die;
+  for ($time,$perl_path,$shortdistro,$ok) {
+    s!\&!\&amp;!g;
+    s!"!&quot;!g;
+    s!<!&lt;!g;
+    s!>!&gt;!g;
+  }
   print $fh qq{<distro time="$time" perl="$perl_path" distro="$shortdistro" ok="$ok">};
   print $fh $log;
   print $fh "</distro>\n";
@@ -54,21 +61,25 @@ we expect the data for exactly this distro. $1 is again the distro.
 
 =cut
 
-our $HTMLSPANSTUFF = qr/(?:<[^<>]+>)+/;
+our $HTMLSPANSTUFF = qr/(?:<[^<>]+>)*/;
 {
-  my %N;
-  my %S;
-  my @distros = uniq map { $S{$_}++; $_ } /^  CPAN\.pm: Going to build (.*)/mg;
-  while (my $d = pop @distros) {
-    if ($S{$d} == 2) {
-      m/^  CPAN\.pm: Going to build $d/gc;
-      warn sprintf "FOUND FIRSTMATCH %s at %d\n", $d, pos $_;
-    }
-    my $shortdistro = $d;
-    $shortdistro =~ s!^[A-Z]/[A-Z][A-Z]/!!;
-    if (
-        s/
-          (\G[\s\S]+)
+  my @logs = ($_);
+  my @residua;
+  while (my $_ = shift @logs) {
+    my @distros = uniq /^  CPAN\.pm: Going to build (.*)/mg;
+    warn sprintf(
+                 "NEW LOG length %d, unprocessed logs ATM: %d, expected distros here: %d",
+                 length($_),
+                 scalar(@logs),
+                 scalar(@distros),
+                );
+    sleep 1;
+    while (my $d = pop @distros) {
+      # my $d = splice @distros, int(scalar(@distros)/2), 1;
+      my $shortdistro = $d;
+      $shortdistro =~ s!^[A-Z]/[A-Z][A-Z]/!!;
+      if (
+          s/
           (
           <span[^<>]+>
           Running[ ](?:install|make|Build)[ ]for[ ]\Q$d\E\n
@@ -78,44 +89,27 @@ our $HTMLSPANSTUFF = qr/(?:<[^<>]+>)+/;
           ^$HTMLSPANSTUFF[ ]{2}(?:\Q$shortdistro\E)\n
           $HTMLSPANSTUFF[ ]{2}.+\s+--\s+((?:NOT\s)?OK)\n
           <\/span>
-         )/$1/mx
-       ) {
-      my($log,$ok) = ($2,$3);
-      warn sprintf "FOUND: %s\n", $d, $S{$d}, length($log);
-      mystore($shortdistro,$log,$ok);
-    } else {
-      warn "NOT found: $d\n";
-      $N{$d}++;
-    }
-  }
-  pos($_) = 0;
-  map {$N{$_}++} />Running \S+ for ([A-Z]\/.*)/mg;
-  for my $d (keys %N) {
-    my $shortdistro = $d;
-    $shortdistro =~ s!^[A-Z]/[A-Z][A-Z]/!!;
-    if (
-        s/
-          (
-          <span[^<>]+>
-          Running[ ](?:install|make|Build)[ ]for[ ]\Q$d\E\n
-          [\s\S]+\n
-          ^$HTMLSPANSTUFF[ ]{2}(?:\Q$shortdistro\E)\n
-          $HTMLSPANSTUFF[ ]{2}.+\s+--\s+((?:NOT\s)?OK)\n
-          <\/span>
          )//mx
-       ) {
-      my($log,$ok) = ($1,$2);
-      warn sprintf "FINALLY FOUND: %s\n", $d, $S{$d}, length($log);
-      mystore($shortdistro,$log,$ok);
-    } else {
-      warn "AGAIN NOT FOUND: $d\n";
-    }
-  }
+         ) {
+        my $log = $1;
+        my $ok  = $2;
+        my @distros_under = uniq $log =~ /^  CPAN\.pm: Going to build (.*)/mg;
+        if (@distros_under == 1) {
+          # warn sprintf "FOUND: %s (%d)\n", $d, length($log);
+          mystore($shortdistro,$log,$ok);
+        } elsif (length $_ == 0) { # exhausted
+          push @residua, $log;
+        } else {
+          push @logs, $log;
+        }
+      }
+    } # while @distros
+    push @residua, $_ if length $_;
+    open my $rfh, ">", "$outdir/residuum.yml" or die;
+    print $rfh YAML::Syck::Dump(\@residua);
+    close $rfh or die;
+  } # while @logs
 }
-
-open my $rfh, ">", "$outdir/residuum.txt" or die;
-print $rfh $_;
-close $rfh or die;
 
 
 =pod
@@ -185,6 +179,29 @@ become extremely slow. 11 hourse between the two timestamps:
 
 Ah, this was an endless loop in CPAN.pm and DateTime-Util-Astro was
 built again and again.
+
+
+THE NEW ALGORITHM:
+
+Start a new array @logs which starts out as ($_). Cut a
+single-distro-log out (Matrushka2). If it does not contain further
+logs, write it to disk, otherwise push it onto @logs. Continue until
+you have tried all candidates.
+
+The game ends when we reach the end of @logs. Then @logs will be an
+array of residua which we shall dump for further considerations.
+
+BUG3
+
+encoding not clear on AWRIGLEY/HTML-Summary-0.017 and illegal
+codepoint 27
+
+I cannot force test scripts to declare the encoding they send me. I
+must live with the fact that I do not know it. Nonetheless I'm
+expected to present it verbatim on a webpage.
+
+
+
 
 
 =cut
