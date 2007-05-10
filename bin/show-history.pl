869139aa5672884e9961db2ecd9ba40b9014d968
Andreas König <andk@cpan.org>
2007-05-10 02:58:48 +0000

    r2408@k75:  k | 2007-05-10 04:58:31 +0200  break the brake
    git-svn-id: https://pause.perl.org:5460/svn/cpanpm/trunk@1829 81f69c26-07c4-0310-b1c3-f486c8728884

--- bin/show-history.pl
+++ bin/show-history.pl
@@ -1,5 +1,20 @@
 #!/usr/local/bin/perl
 
+=pod
+
+ $0 [--logs=logsdir] distro
+
+looks at all dirents in logsdir (which defaults to C<./logs>). If an
+entry looks like a megainstall directory it is probed for distro XML
+file and if it is there, one line is output.
+
+ $0 --logs=megainstalldir
+
+looks at all dirents in this megainstall directory that end in C<.xml>
+and outputs a line for each.
+
+=cut
+
 
 use strict;
 use warnings;
@@ -10,7 +25,7 @@ use XML::LibXML;
 our %Opt;
 
 sub Usage (){
-  "Usage: $0 [--logs=logsdirector] distro";
+  "Usage: $0 [--logs=logsdirector] [distro]";
 }
 
 GetOptions(\%Opt, "logs=s"
@@ -19,15 +34,21 @@ GetOptions(\%Opt, "logs=s"
 my $logdir = $Opt{logs} || "$FindBin::Bin/../logs";
 opendir my $dh, $logdir or die "cannot opendir '$logdir': $!";
 
-my $distro = shift or die Usage;
-$distro =~ s!\.(tar.gz|tgz|tar.bz2|tbz|zip)?$!.xml!;
-$distro =~ s|$|.xml| unless $distro =~ /\.xml$/;
-$distro =~ s|/|!|g;
+my $distro = shift;
+if ($distro) {
+  $distro =~ s!\.(tar.gz|tgz|tar.bz2|tbz|zip)?$!.xml!;
+  $distro =~ s|$|.xml| unless $distro =~ /\.xml$/;
+  $distro =~ s|/|!|g;
+}
 my $p = XML::LibXML->new;
 for my $dirent (sort readdir $dh) {
-  next unless $dirent =~ /^megainstall\.(\d+T\d+)\.d$/;
+  next if $dirent =~ /^\./;
+  my $abs = "$logdir/$dirent";
+  next unless $abs =~ /(?:^|\/)megainstall\.(\d+T\d+)\.d(?:\/|$)/;
   my $time = $1;
-  my $xfile = sprintf "%s/%s/%s", $logdir, $dirent, $distro;
+  my $xfile = sprintf "%s/%s", $logdir, $dirent;
+  $xfile .= "/$distro" if $distro;
+  next unless $xfile =~ /\.xml$/;
   next unless -e $xfile;
   my $xml = $p->parse_file($xfile);
   my($ok,$seq,$perl,$patchlevel,$branch);
@@ -35,11 +56,16 @@ for my $dirent (sort readdir $dh) {
   $seq = $xml->findvalue("/distro/\@seq") || 0;
   $perl = $xml->findvalue("/distro/\@perl");
   ($branch,$patchlevel) = $perl =~ m|/installed-perls/(.*?)/p.*?/perl-5.*?@(\d+)|;
-  printf("%s %-12s %6d %4d %s\n",
+  printf("%s %-10s %6d %4d %-6s",
          $time,
          $branch,
          $patchlevel,
          $seq,
          $ok,
         );
+  if ($distro) {
+    print "\n";
+  } else {
+    print " $dirent\n";
+  }
 }
