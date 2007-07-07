fff727f5e599d33c15c90b0efac20846da4cb701
Andreas König <andk@cpan.org>
2007-07-07 07:54:54 +0000

    r2667@k75:  k | 2007-07-07 07:08:21 +0200  determining the end is quite tricky; measuring where we lost our time
    git-svn-id: https://pause.perl.org:5460/svn/cpanpm/trunk@1960 81f69c26-07c4-0310-b1c3-f486c8728884

--- bin/colorout-to-dir-2.pl
+++ bin/colorout-to-dir-2.pl
@@ -15,9 +15,10 @@ use Encode qw(decode);
 use Encode::Detect ();
 use File::Path qw(mkpath);
 use List::MoreUtils qw(uniq);
-use Time::HiRes qw(sleep);
+use Time::HiRes qw(sleep time);
 use YAML::Syck;
 
+our $start = time;
 our($perl_path) = m|(/home\S+/installed-perls/(?:.*?)/p.*?/perl-5.*?@(?:\d+))|;
 our $outdir = $ARGV;
 $outdir =~ s/.out$/.d/ or die;
@@ -49,50 +50,77 @@ sub mystore ($$$$){
   sleep 1/16;
 }
 
+sub measure ($) {
+  warn sprintf "[%s]time since last measure[%.4f]\n", shift, time - $start;
+  sleep 1;
+  $start = time;
+}
+
 # the first part is a duplication of colorterm-to-html.pl which I
 # wrote for my Munich talk:
+
 s!\&!\&amp;!g;
-sleep 1;
+measure("amp");
 s!"!&quot;!g;
-sleep 1;
+measure("quot");
 s!<!&lt;!g;
-sleep 1;
+measure("lt");
 s!>!&gt;!g;
-sleep 1;
+measure("gt");
 s!\e\[1;3[45](?:;\d+)?m(.*?)\e\[0m!<span style="color: blue">$1</span>!sg;
-sleep 1;
+measure("blue");
 s!\e\[1;31(?:;\d+)?m(.*?)\e\[0m!<span style="color: red">$1</span>!sg;
-sleep 1;
+measure("red");
 #s!\n!<br/>\n!g;
 s!\r\n!\n!g;
-sleep 1;
-s!.+\r!!g;
-sleep 1;
+measure("CRLF");
 
 {
   my @lines = split /\n/, $_;
+  measure("split");
   my %seq; # $seq{$shortdistro} = [];
   my @longdistro;
   my @shortdistro;
   my $i = 0;
  LINE: while (defined($_ = shift @lines)) {
+    s!.+?\r!!g;
     if (m|<span[^<>]+>Running make for ([A-Z]/[A-Z][A-Z]/)([\w-]+/.+)|) {
       $DB::single=1;
       push @shortdistro, $2;
       push @longdistro, "$1$2";
       $seq{$shortdistro[-1]} ||= [];
-    } elsif (m|[ ]{2}\Q$shortdistro[-1]\E|
-             && $lines[0] =~ /[ ]{2}.+install.+\s+--\s+((?:NOT\s)?OK|NA)/
-            ) {
-      my $ok = $1;
-      $i++;
+    } elsif (m|[ ]{2}\Q$shortdistro[-1]\E|) {
       push @{$seq{$shortdistro[-1]}}, $_;
-      push @{$seq{$shortdistro[-1]}}, shift @lines;
-      my $log = join "", map { "$_\n" } @{$seq{$shortdistro[-1]}};
-      mystore($shortdistro[-1],$log,$ok,$i);
-      delete $seq{$shortdistro[-1]};
-      pop @longdistro;
-      pop @shortdistro;
+      my $end = 0;
+      my $ok;
+      if ($lines[0] =~ /[ ]{2}.+install.+\s+--\s+((?:NOT\s)?OK|NA)/) {
+        $ok = $1;
+        push @{$seq{$shortdistro[-1]}}, shift @lines;
+        $end = 1;
+      } elsif ($lines[0] =~ /[ ]{2}.+\s+--\s+((?:NOT\s)?OK|NA)/) {
+        $ok = $1;
+        push @{$seq{$shortdistro[-1]}}, shift @lines;
+        if ($lines[0] =~ />Running.*test/
+            && $lines[1] =~ />[ ]{2}/) {
+          push @{$seq{$shortdistro[-1]}}, shift @lines;
+          push @{$seq{$shortdistro[-1]}}, shift @lines;
+          $end=1;
+        }
+        if ($lines[0] =~ />Running.*install/
+            && $lines[1] =~ />[ ]{2}/) {
+          push @{$seq{$shortdistro[-1]}}, shift @lines;
+          push @{$seq{$shortdistro[-1]}}, shift @lines;
+          $end=1;
+        }
+      }
+      if ($end) {
+        $i++;
+        my $log = join "", map { "$_\n" } @{$seq{$shortdistro[-1]}};
+        mystore($shortdistro[-1],$log,$ok,$i);
+        delete $seq{$shortdistro[-1]};
+        pop @longdistro;
+        pop @shortdistro;
+      }
       next LINE;
     }
     push @{$seq{$shortdistro[-1]}}, $_;
