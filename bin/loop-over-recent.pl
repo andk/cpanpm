54708e46adc7fac6a83b08c0c6db22916e14367e
Andreas König <andk@cpan.org>
2007-08-12 03:05:19 +0000

    r2999@k75:  k | 2007-08-12 05:03:43 +0200  trivia
    git-svn-id: https://pause.perl.org:5460/svn/cpanpm/trunk@2130 81f69c26-07c4-0310-b1c3-f486c8728884

--- bin/loop-over-recent.pl
+++ bin/loop-over-recent.pl
@@ -11,6 +11,8 @@ my $recent = "/home/ftp/pub/PAUSE/authors/id/RECENT-2d.yaml";
 my $otherperls = "$0.otherperls";
 my $statefile = "$ENV{HOME}/.cpan/loop-over-recent.state";
 
+my $rx = qr!\.(tar.gz|tar.bz2|zip|tgz|tbz)$!;
+
 my @perls = qw(); # we'll fill it at runtime!
 
 my $max_epoch_worked_on = 0;
@@ -42,6 +44,7 @@ ITERATION: while () {
     }
     close $fh;
   }
+  shift @perls while @perls && ! -x $perls[0];
   if (open my $fh2, $otherperls) {
     while (<$fh2>) {
       chomp;
@@ -52,7 +55,7 @@ ITERATION: while () {
     }
   }
   my($recent_data) = YAML::Syck::LoadFile($recent);
-  $recent_data = [ grep { $_->{path} =~ m!\.(tar.gz|tar.bz2|\.zip)$! } @$recent_data ];
+  $recent_data = [ grep { $_->{path} =~ $rx } @$recent_data ];
   {
     my %seen;
     $recent_data = [ grep { my $d = CPAN::DistnameInfo->new($_->{path});
@@ -60,15 +63,20 @@ ITERATION: while () {
                           } @$recent_data ];
   }
  UPLOADITEM: for my $upload (reverse @$recent_data) {
-    next unless $upload->{path} =~ m!\.(tar.gz|tar.bz2|\.zip)$!;
+    next unless $upload->{path} =~ $rx;
     next unless $upload->{type} eq "new";
     # never install stable reporters, they are most probably older
     # than we are
     next if $upload->{path} =~ m!DAGOLDEN/CPAN-Reporter-0\.\d+\.tar\.gz!;
-    if ($upload->{epoch} <= $max_epoch_worked_on) {
-      warn "Skipping already handled $upload->{path}\n" unless $have_warned{$upload->{path}}++;
+    if ($upload->{epoch} < $max_epoch_worked_on) {
+      warn "Already done: $upload->{path}\n" unless $have_warned{$upload->{path}}++;
       sleep 0.1;
       next UPLOADITEM;
+    } elsif ($upload->{epoch} == $max_epoch_worked_on) {
+      if ($comboseen{"ALL",$upload->{path}}) {
+        next UPLOADITEM;
+      }
+      warn "Maybe already worked on, we'll retry them: $upload->{path}";
     }
     {
       open my $fh, ">", $statefile or die "Could not open >$statefile\: $!";
@@ -77,23 +85,27 @@ ITERATION: while () {
     }
     $max_epoch_worked_on = $upload->{epoch};
     my $epoch_as_localtime = scalar localtime $upload->{epoch};
-    for my $perl (@perls) {
-      my $perl_version = do { open my $fh, "$perl -e \"print \$]\" |" or die "Couldnt open $perl: $!";
-                              <$fh>;
-                            };
-      my $combo = "|-> '$perl'(=$perl_version) <-> '$upload->{path}' <-> '$epoch_as_localtime'(=$upload->{epoch}) <-|";
+  PERL: for my $perl (@perls) {
+      my $perl_version =
+          do { open my $fh, "$perl -e \"print \$]\" |" or die "Couldnt open $perl: $!";
+               <$fh>;
+             };
+      my $combo = "|-> '$perl'(=$perl_version) <-> '$upload->{path}' ".
+          "<-> '$epoch_as_localtime'(=$upload->{epoch}) <-|";
       if (0) {
-      } elsif ($comboseen{$perl,$upload->{path}}++){
+      } elsif ($comboseen{$perl,$upload->{path}}){
         warn "dead horses combo $combo";
-        sleep 5;
-        next UPLOADITEM;
+        sleep 2;
+        next PERL;
       } else {
         warn "\n\n$combo\n\n\n";
         my $abs = dirname($recent) . "/$upload->{path}";
-        while (! -f $abs) {
+        {
           local $| = 1;
-          print ".";
-          sleep 5;
+          while (! -f $abs) {
+            print ",";
+            sleep 5;
+          }
         }
         $ENV{PERL_MM_USE_DEFAULT} = 1;
         my @system = (
@@ -110,15 +122,20 @@ ITERATION: while () {
           warn "      $perl\n";
           warn "      $upload->{path}\n";
           warn "ATTN-ATTN-ATTN-ATTN-ATTN-ATTN-ATTN-ATTN-ATTN-ATTN-ATTN-ATTN\n";
- 	  sleep 60;
+ 	  sleep 30;
         }
+        $comboseen{$perl,$upload->{path}} = $upload->{epoch};
       }
     }
+    $comboseen{"ALL",$upload->{path}} = $upload->{epoch};
     next ITERATION; # see what is new before simply going through the ordered list
   }
-  # guaratee a minimum of 60 seconds per loop
-  if (time - $iteration_start < 60) {
-    sleep 60 - (time - $iteration_start);
+  my $minimum_time_per_loop = 30;
+  if (time - $iteration_start < $minimum_time_per_loop) {
+    sleep $iteration_start + $minimum_time_per_loop - time;
+  }
+  for my $k (keys %comboseen) {
+    delete $comboseen{$k} if $comboseen{$k} < time - 60*60*24*2;
   }
   { local $| = 1; print "."; }
 }
--- bin/recent.pl
+++ bin/recent.pl
@@ -4,6 +4,9 @@ use YAML::Syck;
 
 my $statefile = "$ENV{HOME}/.cpan/loop-over-recent.state";
 my $max_epoch_worked_on = 0;
+
+my $rx = qr!\.(tar.gz|tar.bz2|zip|tgz|tbz)$!; # see also loop-over...
+
 if (-e $statefile) {
   local $/;
   my $state = do { open my $fh, $statefile or die "Couldn't open '$statefile': $!";
@@ -15,7 +18,7 @@ if (-e $statefile) {
 }
 my $recent = YAML::Syck::LoadFile "/home/ftp/pub/PAUSE/authors/id/RECENT-2d.yaml";
 ITEM: for my $item (@$recent) {
-  next unless $item->{path} =~ m!\.(tar.gz|tar.bz2|\.zip)$!;
+  next unless $item->{path} =~ $rx;
   next unless $item->{type} eq "new";
   printf "%1s %s %s\n", ($max_epoch_worked_on && $max_epoch_worked_on == $item->{epoch}) ?
       "*" : "", scalar localtime $item->{epoch}, substr($item->{path},5);
