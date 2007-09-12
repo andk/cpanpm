d9e00ad4530398936e4354c63bf3dea1e4710597
Andreas König <andk@cpan.org>
2007-09-12 04:39:19 +0000

    r3155@k75:  k | 2007-09-12 06:38:43 +0200  fix VERSION on DeferedCode
    git-svn-id: https://pause.perl.org:5460/svn/cpanpm/trunk@2224 81f69c26-07c4-0310-b1c3-f486c8728884

--- lib/CPAN/DeferedCode.pm
+++ lib/CPAN/DeferedCode.pm
@@ -7,7 +7,7 @@ use overload fallback => 1, map { ($_ => 'run') } qw/
     bool "" 0+
 /;
 
-$VERSION = sprintf "%.6f", substr(q$Rev$,4)/1000000 + 5.4;
+$VERSION = "5.50";
 
 sub run {
     $_[0]->();
