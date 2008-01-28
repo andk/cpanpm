d51b9350dbf083ba910e010ce40f11f04ea4be63
Andreas König <andk@cpan.org>
2008-01-28 21:25:42 +0000

    r3940@k75:  k | 2008-01-28 22:23:06 +0100  removing all the crappy bugs to make way free for the KISS version
    git-svn-id: https://pause.perl.org:5460/svn/cpanpm/trunk@2627 81f69c26-07c4-0310-b1c3-f486c8728884

--- bin/ls-build+tmp.pl
+++ bin/ls-build+tmp.pl
@@ -10,14 +10,22 @@ such a way that we can say which distro dropped something.
 use strict;
 use warnings;
 
+
 use Getopt::Long;
+use Time::HiRes qw(sleep);
+
+my %ignore = map { ;"/tmp/$_" => 1; } ".X121-lock", ".UUID_NODEID";
+
 my %Config = (
+              cleanup => 30,
              );
 
 GetOptions(\my %config,
            (map { "$_=s" } keys %Config),
            "debug!",
           ) or die;
+
+
 while (my($k,$v) = each %config) {
   $Config{$k} = $v;
 }
@@ -28,45 +36,28 @@ for my $dir (qw(/tmp /home/sand/.cpan/build /home/k/.cpan/build)) {
   opendir my $dh, $dir or die "Couldn't opendir: $!";
   push @all, map { "$dir/$_" } readdir $dh;
 }
+@all = grep { ! exists $ignore{$_} } @all;
 my @rall = sort { $a->[1] <=> $b->[1] }
     map { [$_, (stat$_)[9]] } @all;
-my @lookback;
 DIRENT: for my $dirent (@rall) {
-  push @lookback, $dirent;
-  if ($dirent->[0] eq $rall[-1][0] || @lookback>=3) {
-    if (
-        $lookback[-3][0] =~ m|^/tmp|
-       ) {
-      if (
-          $lookback[0][0] !~ m|^/tmp|
-          &&
-          $lookback[1][0] !~ m|^/tmp|
-          &&
-          $lookback[-2][0] !~ m|^/tmp|
-          &&
-          $lookback[-1][0] !~ m|^/tmp|
-         ) {
-        shift @lookback while $lookback[2][0] !~ m|^/tmp|;
-        while (my $de = shift @lookback) {
-          my @t = localtime($de->[1]);
-          $t[5]+=1900;
-          $t[4]++;
-          my $sigil = -d $de->[0] ? "/" : "";
-          printf "%04d-%02d-%02dT%02d:%02d:%02d %s%s\n", @t[5,4,3,2,1,0], $de->[0], $sigil;
-        }
-        print "---\n";
-      } else {
-        if ($Config{debug}) {
-          my @t = localtime($dirent->[1]);
-          $t[5]+=1900;
-          $t[4]++;
-          my $sigil = -d $dirent->[0] ? "/" : "";
-          printf "%04d-%02d-%02dT%02d:%02d:%02d %s%s\n", @t[5,4,3,2,1,0], $dirent->[0], $sigil;
-          # printf "%s\n", $dirent->[0];
-        }
+  if (my $cleanup_time = $Config{cleanup}) {
+    if ($dirent->[0] =~ m|^/tmp/........|) {
+      my $age = sprintf "%.4f", -M $dirent->[0];
+      if ($age > $cleanup_time) {
+        require File::Path;
+        warn "Going to rmtree '$dirent->[0]' age[$age]\n";
+        sleep 0.2;
+        File::Path::rmtree($dirent->[0]);
         next DIRENT;
       }
     }
   }
-  shift @lookback if @lookback > 50;
+  if ($Config{debug}) {
+    my @t = localtime($dirent->[1]);
+    $t[5]+=1900;
+    $t[4]++;
+    my $sigil = -d $dirent->[0] ? "/" : "";
+    printf "%04d-%02d-%02dT%02d:%02d:%02d %6d %s%s\n", @t[5,4,3,2,1,0], -s $dirent->[0], $dirent->[0], $sigil;
+    # printf "%s\n", $dirent->[0];
+  }
 }
