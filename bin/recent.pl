61dd1b0810dd2d3392d1912e4bb8088fb2226a96
Andreas König <andk@cpan.org>
2008-06-06 01:59:56 +0000

    paint the star that makrs currently processing item also in the case that current timestamp falls between two displayable items
    git-svn-id: https://pause.perl.org:5460/svn/cpanpm/trunk@3169 81f69c26-07c4-0310-b1c3-f486c8728884

--- bin/recent.pl
+++ bin/recent.pl
@@ -1,8 +1,29 @@
 #!/usr/bin/perl
 
+=head1 NAME
+
+recent - 
+
+=head1 SYNOPSIS
+
+ watch -n 20 'perl ~k/sources/CPAN/GIT/trunk/bin/recent.pl -n 15;perl -e\$c=800\;while\(--\$c\){print\ rand\(80\)\<1\?\"_\":\"\ \"} '
+
+=head1 DESCRIPTION
+
+Show most recent uploads according to the RECENT file and mark the
+currently processing one (according to ~/.cpan/loop-over-recent.state
+with a star.
+
+=cut
+
 use CPAN::DistnameInfo;
+use Getopt::Long;
 use YAML::Syck;
 
+our %Opt;
+GetOptions(\%Opt,
+          "n=i",
+          );
 use lib "/home/k/dproj/PAUSE/wc/lib/";
 use PAUSE; # loads File::Rsync::Mirror::Recentfile for now
 
@@ -31,11 +52,38 @@ my $recent_events = $rf->recent_events;
   my %seen;
   $recent_events = [ grep { my $d = CPAN::DistnameInfo->new($_->{path});
                             !$seen{$d->dist}++
+                                && $_->{path} =~ $rx
+                                    && $_->{type} eq "new";
+                            
                           } @$recent_events ];
 }
-ITEM: for my $item (@$recent_events) {
-  next unless $item->{path} =~ $rx;
-  next unless $item->{type} eq "new";
-  printf "%1s %s %s\n", ($max_epoch_worked_on && $max_epoch_worked_on == $item->{epoch}) ?
-      "*" : "", scalar localtime $item->{epoch}, substr($item->{path},5);
+my $current_is_marked = 0;
+my $count = 0;
+ITEM: for my $i (0..$#$recent_events) {
+  my $item = $recent_events->[$i];
+  my $mark = "";
+  if (!$current_is_marked) {
+    if ($max_epoch_worked_on) {
+      if ($max_epoch_worked_on == $item->{epoch}) {
+        $mark = "*";
+        $current_is_marked = 1;
+      } elsif ($max_epoch_worked_on < $item->{epoch}
+               && $#$recent_events > $i
+               && $max_epoch_worked_on > $recent_events->[$i+1]->{epoch}) {
+        printf "%1s %s\n", "*", scalar localtime $recent_events->[$i+1]->{epoch};
+        $current_is_marked = 1;
+      }
+    }
+  }
+  printf "%1s %s %s\n", $mark, scalar localtime $item->{epoch}, substr($item->{path},5);
+  if ($Opt{n} && ++$count>=$Opt{n}) {
+    last ITEM;
+  }
 }
+
+__END__
+
+# Local Variables:
+# mode: cperl
+# cperl-indent-level: 2
+# End:
