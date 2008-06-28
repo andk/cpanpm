7d52b76bd8fe78c735ec6fc35d6c35a1e75ddaea
Andreas König <andk@cpan.org>
2008-06-28 06:42:19 +0000

    adjust rsyncing: do both authors and modules in one loop, introduce new options
    git-svn-id: https://pause.perl.org:5460/svn/cpanpm/trunk@3221 81f69c26-07c4-0310-b1c3-f486c8728884

--- bin/rsync-over-recentfile.pl
+++ bin/rsync-over-recentfile.pl
@@ -14,7 +14,7 @@ field that tells us in human readable language what this tree is about.
 and a field mirroring_from: consisting of host, module, path,
 authentification_needed, 
 
-add interval (2d) of the whole file to the metadata.
+add interval (2d,1W) of the whole file to the metadata.
 
 keep the very first item in the got_at hash. we cannot know if this
 second has been fully processed. On the next loop we need to look into
@@ -23,16 +23,18 @@ current code would delete this item. UPDATE: when removing at the end
 of the loop just change "< time ..." to "< $max_epoch ..." DONE
 
 Q: sorted by epoch? Who has to sort when how often? A: nobody! The
-mechanism is event based and the array is only running push and shift.
-It is a journal and is itself NOT rsynced but in fact rewritten by
-every slave. [find literature about journaling fs!] Do slaves inherit
-the timestamp from the master or do they write their own? I think they
-inherit it (Update 2007-10-21 akoenig : or we do not ever promise that
-the timestamps are sorted or mirror the sequence of events or we make
-them floating point numbers so they become uniq and can be treated as
-hash keys). But then they must not mirror events in a later second
-before all seconds below are finished (Update 2007-10-21 akoenig :
-this can be relaxed).
+mechanism is "event" based and the array is only running push and
+shift. It is a journal (that throws memory away based on an interval)
+and is itself NOT rsynced but in fact rewritten by every slave.
+[journaling fs are not so much related because they throw away after
+having written] Do slaves inherit the timestamp from the master or do
+they write their own? I think they inherit it (Update 2007-10-21
+akoenig : or we do not ever promise that the timestamps are sorted or
+mirror the sequence of events or we make them floating point numbers
+so they become uniq and can be treated as hash keys [this I like!]).
+But then they must not mirror events in a later second before all
+seconds below are finished (Update 2007-10-21 akoenig : this can be
+relaxed).
 
       M       St1       St2       St3       St4
 
@@ -100,7 +102,6 @@ use strict;
 use warnings;
 use File::Basename qw(dirname);
 use File::Spec;
-use File::Temp ();
 use Getopt::Long qw(GetOptions);
 use List::Util qw(min);
 use Time::HiRes qw(sleep);
@@ -110,24 +111,12 @@ use PAUSE; # loads File::Rsync::Mirror::Recentfile for now
 
 our %Opt;
 GetOptions(\%Opt,
+           "use_interval=i",
            "loops=i",
+           "verbose!",
           ) or die;
 
-my $rf = File::Rsync::Mirror::Recentfile->new
-    (
-     canonize => "naive_path_normalize",
-     localroot => "/home/ftp/pub/PAUSE/authors/id/",
-     interval => q(2d),
-     remote_host => "pause.perl.org",
-     remote_module => "authors",
-     remote_dir => "id",
-     rsync_options => {
-                       compress => 1,
-                       'rsync-path' => '/usr/bin/rsync',
-                      }
-    );
-
-my $max_epoch_ever = 0;
+my %max_epoch_ever;
 
 my %got_at;
 my $print_leading_newline = 0;
@@ -136,96 +125,132 @@ ITERATION: while () {
   last if $Opt{loops} && $loop++ >= $Opt{loops};
   my $iteration_start = time;
 
-  my $trecentfile = eval {$rf->get_remote_recentfile_as_tempfile("2d");};
-  if ($@) {
-    warn sprintf "Warning: %s", $@;
-    sleep 5;
-    next ITERATION;
-  }
-  my($recent_data) = $rf->recent_events_from_file($trecentfile);
-  my @error;
-  my $i = 0;
-  my $total = @$recent_data;
- UPLOADITEM: for my $recent_event (reverse @$recent_data) {
-    $i++;
-    if ($recent_event->{type} eq "new"){
-      my $must_get;
-      if ($recent_event->{epoch} < $max_epoch_ever) {
-        next UPLOADITEM;
-      } elsif ($recent_event->{epoch} == $max_epoch_ever) {
-        unless ($got_at{$recent_event->{path}}++) {
+  for my $rmodule (qw(authors modules)) {
+    my $rf = File::Rsync::Mirror::Recentfile->new
+        (
+         canonize => "naive_path_normalize",
+         filenameroot => "TESTPLEASEIGNORE",
+         ignore_link_stat_errors => 1,
+         interval => q(1W),
+         localroot => "/home/ftp/pub/PAUSE/$rmodule",
+         remote_dir => "",
+         remote_host => "pause.perl.org",
+         remote_module => $rmodule,
+         rsync_options => {
+                           # intenionally not using archive=>1 because it contains "r"
+                           compress => 1,
+                           'rsync-path' => '/usr/bin/rsync',
+                           links => 1,
+                           times => 1,
+                           'omit-dir-times' => 1,
+                           checksum => 1,
+                          },
+         verbose => $Opt{verbose},
+        );
+
+    my $trecentfile = eval {$rf->get_remote_recentfile_as_tempfile();};
+    if ($@) {
+      warn sprintf "Warning: %s", $@; # XXX need a logging mechanism
+      sleep 5;
+      next ITERATION;
+    }
+    my($recent_data) = $rf->recent_events_from_file($trecentfile);
+    my @error;
+    my $i = 0;
+    my $total = @$recent_data;
+  UPLOADITEM: for my $recent_event (reverse @$recent_data) {
+      $i++;
+      if ($recent_event->{type} eq "new"){
+        my $must_get;
+        $max_epoch_ever{$rmodule} ||= 0;
+        if ($Opt{use_interval} && $recent_event->{epoch}+$Opt{use_interval} < time) {
+          next UPLOADITEM;
+        } elsif ($recent_event->{epoch} < $max_epoch_ever{$rmodule}) {
+          next UPLOADITEM;
+        } elsif ($recent_event->{epoch} == $max_epoch_ever{$rmodule}) {
+          unless ($got_at{$recent_event->{path}}++) {
+            $must_get++;
+          }
+        } else {
           $must_get++;
         }
+        if ($must_get) {
+          my $dst = $rf->local_event_path($recent_event->{path});
+          my $doing = -e $dst ? "Syncing" : "Getting";
+          {
+            printf(
+                   "%s%s (%d/%d) %s\n",
+                   $print_leading_newline ? "\n" : "",
+                   $doing,
+                   $i,
+                   $total,
+                   $recent_event->{path},
+                  );
+            $print_leading_newline = 0;
+          }
+          eval { $rf->mirror_path($recent_event->{path}) };
+          if ($@) {
+            push @error, $@;
+            sleep 1;
+            next UPLOADITEM;
+          }
+          $got_at{$recent_event->{path}} = $recent_event->{epoch};
+        }
+      } elsif ($recent_event->{type} eq "delete") {
+        # note that we should not delete files on CPAN immediately
+        # before the indexes are adjusted. This is probably a bug in
+        # the CPAN system but the problem might be quite common.
+
+        # the mirror master should always adjust his indexes before
+        # actually deleting files. Or at least rewrite the index
+        # immediately after a couple of deletes. Fortunately the cpan
+        # system usually only deletes files that are in no index
+        # anymore (but people make mistakes)
       } else {
-        $must_get++;
+        warn "Warning: invalid upload type '$recent_event->{type}'"; # XXX logging
       }
-      if ($must_get) {
-        my $dst = $rf->local_event_path($recent_event->{path});
-        my $doing = -e $dst ? "Syncing" : "Getting";
-        {
-          printf(
-                 "%s%s (%d/%d) %s\n",
-                 $print_leading_newline ? "\n" : "",
-                 $doing,
-                 $i,
-                 $total,
-                 $recent_event->{path},
-                );
-          $print_leading_newline = 0;
-        }
-        eval { $rf->mirror_path($recent_event->{path}) };
-        if ($@) {
-          push @error, $@;
-          sleep 1;
-          next UPLOADITEM;
-        }
-        $got_at{$recent_event->{path}} = $recent_event->{epoch};
-      }
-    } else {
-      warn "Warning: invalid upload type '$recent_event->{type}'";
+      $max_epoch_ever{$rmodule} = $recent_event->{epoch} if $recent_event->{epoch} > $max_epoch_ever{$rmodule};
     }
-    $max_epoch_ever = $recent_event->{epoch} if $recent_event->{epoch} > $max_epoch_ever;
-  }
-  if (@error) {
-    # XXX this seems a bit too drastic
-    my $errors = @error;
-    my @disperrors = splice @error, 0, min(10, scalar @error);
-    my $disperrors = @disperrors;
-    warn "Warning: Ran into $errors errors, $disperrors follow:
+    if (@error) {
+      # XXX this seems a bit too drastic
+      my $errors = @error;
+      my @disperrors = splice @error, 0, min(10, scalar @error);
+      my $disperrors = @disperrors;
+      warn "Warning: Ran into $errors errors, $disperrors follow:
 @disperrors
 ";
-    sleep 12;
-    $max_epoch_ever = 0;
-    %got_at = ();
-  } else {
-    for my $k (keys %got_at) {
-      delete $got_at{$k} if $got_at{$k} < $max_epoch_ever - 60*60*24*2;
+      sleep 12;
+      $max_epoch_ever{$rmodule} = 0;
+      %got_at = ();
+    } else {
+      for my $k (keys %got_at) {
+        delete $got_at{$k} if $got_at{$k} < $max_epoch_ever{$rmodule} - 60*60*24*2;
+      }
     }
+
+    # XXX broken: we must do something else when an error happens. I
+    # think we must rewrite the recent file and set the eventtype to
+    # unknown or something. But we must publish *some* recent file
+    # otherwise a single error stops the whole mirroring process. In our
+    # case it was a file that got deleted at the source but it was not
+    # reflected in the RECENT file. We must never believe that the R
+    # file is perfect.
+
+    # Note: by setting the max_time_ever to zero we might also do harm
+    # insofar we hit the parent server to often with all the files that
+    # are OK. We should probably only retry the files that have an
+    # error, like in a nosuccesscount and nosuccesstime and retry rate.
+
+    rename $trecentfile, $rf->recentfile;
   }
 
-  # XXX broken: we must do something else when an error happens. I
-  # think we must rewrite the recent file and set the eventtype to
-  # unknown or something. But we must publish *some* recent file
-  # otherwise a single error stops the whole mirroring process. In our
-  # case it was a file that got deleted at the source but it was not
-  # reflected in the RECENT file. We must never believe that the R
-  # file is perfect.
-
-  # Note: by setting the max_time_ever to zero we might also do harm
-  # insofar we hit the parent server to often with all the files that
-  # are OK. We should probably only retry the files that have an
-  # error, like in a nosuccesscount and nosuccesstime and retry rate.
-
-  rename $trecentfile, $rf->recentfile;
-
-
   my $minimum_time_per_loop = 20;
-  { local $| = 1; print "~"; $print_leading_newline = 1; }
+  { local $| = 1; print "."; $print_leading_newline = 1; }
   if (time - $iteration_start < $minimum_time_per_loop) {
     # last ITERATION;
     sleep $iteration_start + $minimum_time_per_loop - time;
   }
-  { local $| = 1; print "."; $print_leading_newline = 1; }
+  { local $| = 1; print "~"; $print_leading_newline = 1; }
 }
 
 print "\n";
