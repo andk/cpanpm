0d323a0d8d62d9a6115826ac2d84caf9ac99333e
Andreas J. Koenig <andreas.koenig.7os6VVqR@franz.ak.mind.de>
2009-01-11 22:27:33 +0100

    still preparing the release

--- bin/no_CR.pl
+++ bin/no_CR.pl
@@ -2,6 +2,7 @@ while (<>) {
   die "contains CR" if /\cM/;
   next unless /^(\S+)/;
   my $file = $1;
+  next if "SIGNATURE" eq $file and ! -f $file;
   open FH, $file or die "Could not open '$file': $!";
   next if -B $file;
   while (<FH>) {
