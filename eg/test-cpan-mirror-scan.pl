1a88d2deec9b642f165e09260bd1b460f4265e82
David Golden <dagolden@cpan.org>
2009-11-05 10:04:16 -0500

    make mirror scan example actually compile with strict

--- eg/test-cpan-mirror-scan.pl
+++ eg/test-cpan-mirror-scan.pl
@@ -4,8 +4,9 @@ use warnings;
 use CPAN::Mirrors;
 my $cm = CPAN::Mirrors->new( "MIRRORED.BY" );
 $|++;
-@best = $cm->best_mirrors( how_many => 5, verbose => 1,
-  callback => sub { print "." } );
-say "Results:";
-say "  " . $_->url for @best
+my @best = $cm->best_mirrors( 
+  how_many => 5, verbose => 1, callback => sub { print "." } 
+);
+print "Results:\n";
+print "  " . $_->url . "\n" for @best;
 
