andk/cpanpm

commit 27252dd6ffec9727daa3aabb126d7d604185fa80

reanimate_build_dir skips yaml files without corresponding directories

lib/CPAN/Index.pm
@@ -126,7 +126,7 @@ sub reanimate_build_dir {
     my @candidates = map { $_->[0] }
         sort { $b->[1] <=> $a->[1] }
             map { [ $_, -M File::Spec->catfile($d,$_) ] }
-                grep {/\.yml$/} readdir $dh;
+                grep {/(.+)\.yml$/ && -d File::Spec->catfile($d,$1)} readdir $dh;
     unless (@candidates) {
         $CPAN::Frontend->myprint("Build_dir empty, nothing to restore\n");
         return;

lib/CPAN/Distribution.pm
@@ -777,6 +777,7 @@ sub choose_MM_or_MB {
     return $self;
 }
 
+# see also reanimate_build_dir
 #-> CPAN::Distribution::store_persistent_state
 sub store_persistent_state {
     my($self) = @_;

commit 38567f8774968d06ff55b96438ef5a7787cefe63

Report no reanimation even when build_dir empty
This fixes failing t/30shell.t with Expect introduced in
commit 27252dd.

lib/CPAN/Index.pm
@@ -123,74 +123,75 @@ sub reanimate_build_dir {
     my $i = 0;
     my $painted = 0;
     my $restored = 0;
+    my $start = CPAN::FTP::_mytime();
     my @candidates = map { $_->[0] }
         sort { $b->[1] <=> $a->[1] }
             map { [ $_, -M File::Spec->catfile($d,$_) ] }
                 grep {/(.+)\.yml$/ && -d File::Spec->catfile($d,$1)} readdir $dh;
-    unless (@candidates) {
-        $CPAN::Frontend->myprint("Build_dir empty, nothing to restore\n");
-        return;
-    }
-    $CPAN::Frontend->myprint
-        (sprintf("Reading %d yaml file%s from %s/\n",
-                 scalar @candidates,
-                 @candidates==1 ? "" : "s",
-                 $CPAN::Config->{build_dir}
-                ));
-    my $start = CPAN::FTP::_mytime();
-  DISTRO: for $i (0..$#candidates) {
-        my $dirent = $candidates[$i];
-        my $y = eval {CPAN->_yaml_loadfile(File::Spec->catfile($d,$dirent))};
-        if ($@) {
-            warn "Error while parsing file '$dirent'; error: '$@'";
-            next DISTRO;
-        }
-        my $c = $y->[0];
-        if ($c && $c->{perl} && $c->{distribution} && CPAN->_perl_fingerprint($c->{perl})) {
-            my $key = $c->{distribution}{ID};
-            for my $k (keys %{$c->{distribution}}) {
-                if ($c->{distribution}{$k}
-                    && ref $c->{distribution}{$k}
-                    && UNIVERSAL::isa($c->{distribution}{$k},"CPAN::Distrostatus")) {
-                    $c->{distribution}{$k}{COMMANDID} = $i - @candidates;
-                }
+    if ( @candidates ) {
+        $CPAN::Frontend->myprint
+            (sprintf("Reading %d yaml file%s from %s/\n",
+                    scalar @candidates,
+                    @candidates==1 ? "" : "s",
+                    $CPAN::Config->{build_dir}
+                    ));
+      DISTRO: for $i (0..$#candidates) {
+            my $dirent = $candidates[$i];
+            my $y = eval {CPAN->_yaml_loadfile(File::Spec->catfile($d,$dirent))};
+            if ($@) {
+                warn "Error while parsing file '$dirent'; error: '$@'";
+                next DISTRO;
             }
+            my $c = $y->[0];
+            if ($c && $c->{perl} && $c->{distribution} && CPAN->_perl_fingerprint($c->{perl})) {
+                my $key = $c->{distribution}{ID};
+                for my $k (keys %{$c->{distribution}}) {
+                    if ($c->{distribution}{$k}
+                        && ref $c->{distribution}{$k}
+                        && UNIVERSAL::isa($c->{distribution}{$k},"CPAN::Distrostatus")) {
+                        $c->{distribution}{$k}{COMMANDID} = $i - @candidates;
+                    }
+                }
 
-            #we tried to restore only if element already
-            #exists; but then we do not work with metadata
-            #turned off.
-            my $do
-                = $CPAN::META->{readwrite}{'CPAN::Distribution'}{$key}
-                    = $c->{distribution};
-            for my $skipper (qw(
-                                badtestcnt
-                                configure_requires_later
-                                configure_requires_later_for
-                                force_update
-                                later
-                                later_for
-                                notest
-                                should_report
-                                sponsored_mods
-                                prefs
-                                negative_prefs_cache
-                               )) {
-                delete $do->{$skipper};
-            }
-            if ($do->can("tested_ok_but_not_installed")) {
-                if ($do->tested_ok_but_not_installed) {
-                    $CPAN::META->is_tested($do->{build_dir},$do->{make_test}{TIME});
-                } else {
-                    next DISTRO;
+                #we tried to restore only if element already
+                #exists; but then we do not work with metadata
+                #turned off.
+                my $do
+                    = $CPAN::META->{readwrite}{'CPAN::Distribution'}{$key}
+                        = $c->{distribution};
+                for my $skipper (qw(
+                                    badtestcnt
+                                    configure_requires_later
+                                    configure_requires_later_for
+                                    force_update
+                                    later
+                                    later_for
+                                    notest
+                                    should_report
+                                    sponsored_mods
+                                    prefs
+                                    negative_prefs_cache
+                                  )) {
+                    delete $do->{$skipper};
                 }
+                if ($do->can("tested_ok_but_not_installed")) {
+                    if ($do->tested_ok_but_not_installed) {
+                        $CPAN::META->is_tested($do->{build_dir},$do->{make_test}{TIME});
+                    } else {
+                        next DISTRO;
+                    }
+                }
+                $restored++;
             }
-            $restored++;
+            $i++;
+            while (($painted/76) < ($i/@candidates)) {
+                $CPAN::Frontend->myprint(".");
+                $painted++;
+            }
         }
-        $i++;
-        while (($painted/76) < ($i/@candidates)) {
-            $CPAN::Frontend->myprint(".");
-            $painted++;
-        }
+    }
+    else {
+        $CPAN::Frontend->myprint("Build_dir empty, nothing to restore\n");
     }
     my $took = CPAN::FTP::_mytime() - $start;
     $CPAN::Frontend->myprint(sprintf(