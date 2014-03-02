35160b4b5b5b38dc969999aaf10c106778bee337
Andreas Koenig <andk@cpan.org>
2014-03-02 15:27:23 +0100

    Revert "welcome bundles in the mandatory/optional new world"
    This reverts commit 61877afbe717671acb10c7ed3ad18bf949742e24.

--- lib/CPAN/Bundle.pm
+++ lib/CPAN/Bundle.pm
@@ -230,8 +230,7 @@ Going to $meth that.
         $obj->{reqtype} = $self->{reqtype};
         # $obj->$meth();
         # XXX should optional be based on whether bundle was optional? -- xdg, 2012-04-01
-        # A: Sure, what could demand otherwise? --andk, 2013-11-25
-        CPAN::Queue->queue_item(qmod => $obj->id, reqtype => $self->{reqtype}, optional => !$self->{mandatory});
+       CPAN::Queue->queue_item(qmod => $obj->id, reqtype => $self->{reqtype}, optional => '');
     }
 }
 
