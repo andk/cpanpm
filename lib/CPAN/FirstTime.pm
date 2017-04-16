8bfd53d23b208190b06334e5bf82a553ae3d1815
Andreas Koenig <andk@cpan.org>
2017-04-16 11:38:33 +0200

    Fix two typos

--- lib/CPAN/FirstTime.pm
+++ lib/CPAN/FirstTime.pm
@@ -439,7 +439,7 @@ Randomize parameter
 generally be installed except in resource constrained environments.  When this
 policy is true, recommended modules will be included with required modules.
 
-Included recommended modules?
+Include recommended modules?
 
 =item scan_cache
 
@@ -489,7 +489,7 @@ Show all individual modules that have a $VERSION of zero?
 dependencies provide enhanced operation.  When this policy is true, suggested
 modules will be included with required modules.
 
-Included suggested modules?
+Include suggested modules?
 
 =item tar_verbosity
 
