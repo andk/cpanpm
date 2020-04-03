e54bfc762a4f66173f36033a8420e1ff1fa60d8f
Andreas Koenig <andk@cpan.org>
2020-04-03 04:33:13 +0200

    Ready to release 2.28-TRIAL

--- lib/App/Cpan.pm
+++ lib/App/Cpan.pm
@@ -6,7 +6,7 @@ use vars qw($VERSION);
 
 use if $] < 5.008 => 'IO::Scalar';
 
-$VERSION = '1.675';
+$VERSION = '1.676';
 
 =head1 NAME
 
--- lib/CPAN/FirstTime.pm
+++ lib/CPAN/FirstTime.pm
@@ -11,7 +11,7 @@ use File::Spec ();
 use CPAN::Mirrors ();
 use CPAN::Version ();
 use vars qw($VERSION $auto_config);
-$VERSION = "5.5314";
+$VERSION = "5.5315";
 
 =head1 NAME
 
